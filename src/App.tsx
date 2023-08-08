d55a9eaeb3022cf41c3397ba6122b2ceaeeb1767
Matvey <empflow@tutanota.com>
2023-08-08 13:14:44 +0300

    remove everything before attempting to make it actually work

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import ProfileMenuIcon from "./components/ProfileMenuIcon/ProfileMenuIcon";
-import DropdownMenu from "./components/DropdownMenu/DropdownMenu";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <main>
-      <header className="flex items-center px-4 justify-end h-[60px] border-b border-gray-400">
-        <ProfileMenuIcon {...{ setIsMenuOpen }} />
-        <DropdownMenu isOpen={isMenuOpen} />
-      </header>
+      <header className="flex items-center px-4 justify-end h-[60px] border-b border-gray-400"></header>
     </main>
   );
 }
