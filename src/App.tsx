18574a9b1c80493944bc5120aa740588adc8cecd
Matvey <empflow@tutanota.com>
2023-10-28 20:30:55 +0300

    reset project to a bare-bones vite react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-import { useState } from "react";
-import DropdownMenuWrapper from "./components/DropdownMenu/DropdownMenuWrapper";
-
-function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  return (
-    <main>
-      <header className="flex items-center px-4 justify-end h-[60px] border-b border-gray-400">
-        <button onClick={() => setIsMenuOpen((prev) => !prev)}>
-          Toggle menu
-        </button>
-        <DropdownMenuWrapper isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
-      </header>
-    </main>
-  );
+export default function App() {
+  return <main>hello</main>;
 }
-
-export default App;
