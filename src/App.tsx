66e601a0811561767012e4aaabeb34c671e3da08
Matvey <empflow@tutanota.com>
2023-10-29 15:43:14 +0300

    react springs test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { ReactElement, createContext, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
+
+type TBgColor = "red" | "blue";
+
 export default function App() {
-  return <main>hello</main>;
+  const [bgColor, setBgColor] = useState<TBgColor>("red");
+  const [styles, api] = useSpring(() => ({
+    from: { backgroundColor: "yellow" },
+    to: { backgroundColor: "orange" },
+  }));
+
+  // const divPosBind = useDrag(({ offset, movement, velocity: [xVel, yVel] }) => {
+  //   const thresholdVel = 3;
+  //   if (xVel > thresholdVel || yVel > thresholdVel) setFast(true);
+  //   else setFast(false);
+  // });
+
+  return (
+    <main>
+      <animated.div style={styles} className="div"></animated.div>
+      <button onClick={() => api.start()}>change</button>
+    </main>
+  );
 }
