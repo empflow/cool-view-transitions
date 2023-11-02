07cef1108f219bc99bcebecd210f674ddb82368a
Matvey <empflow@tutanota.com>
2023-11-02 12:47:03 +0300

    Revert "react springs test"
    This reverts commit 66e601a0811561767012e4aaabeb34c671e3da08.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,3 @@
-import { ReactElement, createContext, useState } from "react";
-import { useSpring, animated } from "@react-spring/web";
-import { useDrag } from "@use-gesture/react";
-
-type TBgColor = "red" | "blue";
-
 export default function App() {
-  const [bgColor, setBgColor] = useState<TBgColor>("red");
-  const [styles, api] = useSpring(() => ({
-    from: { backgroundColor: "yellow" },
-    to: { backgroundColor: "orange" },
-  }));
-
-  // const divPosBind = useDrag(({ offset, movement, velocity: [xVel, yVel] }) => {
-  //   const thresholdVel = 3;
-  //   if (xVel > thresholdVel || yVel > thresholdVel) setFast(true);
-  //   else setFast(false);
-  // });
-
-  return (
-    <main>
-      <animated.div style={styles} className="div"></animated.div>
-      <button onClick={() => api.start()}>change</button>
-    </main>
-  );
+  return <main>hello</main>;
 }
