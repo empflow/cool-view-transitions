38e40aafe3af4d11b3c265be4fb35d3ac55b908d
Matvey <empflow@tutanota.com>
2023-11-02 12:51:45 +0300

    Revert "reset project to a bare-bones vite react project"
    This reverts commit 18574a9b1c80493944bc5120aa740588adc8cecd.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-export default function App() {
-  return <main>hello</main>;
+import { useState } from "react";
+import DropdownMenuWrapper from "./components/DropdownMenu/DropdownMenuWrapper";
+
+function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <main>
+      <header className="flex items-center px-4 justify-end h-[60px] border-b border-gray-400">
+        <button onClick={() => setIsMenuOpen((prev) => !prev)}>
+          Toggle menu
+        </button>
+        <DropdownMenuWrapper isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+      </header>
+    </main>
+  );
 }
+
+export default App;
